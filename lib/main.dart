5d7b4199ec706152e3cd496f7e624d035170e33e
Edan Levy <62389779+EdanLevy@users.noreply.github.com>
2024-01-11 17:54:09 +0200

    Poc screen created

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-import 'poc/MyHomePage.dart';
+import 'poc/poc_selection_screen.dart';
 import 'firebase_options.dart';
 
 void main() async {
---
+++ lib/poc/poc_selection_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:safrt_eye_app/poc/FirstPage.dart';
+import 'package:safrt_eye_app/poc/SecondPage.dart';
+import 'package:safrt_eye_app/poc/camera.dart';
+import 'package:safrt_eye_app/poc/voice_recognition.dart';
+
+import './LoginRegisterPage.dart';
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Welcome to SafetyEye!',
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 20),
+            PocButtonWidget(title: "Login/Register", page: LoginRegisterPage()),
+            PocButtonWidget(title: "Camera", page: CameraHomeScreen()),
+            PocButtonWidget(title: "Text to speech", page: SpeechScreen()),
+            PocButtonWidget(title: "compression", page: SecondPage(userEmail: "")),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PocButtonWidget extends StatelessWidget {
+  final String title;
+  final Widget page;
+
+  const PocButtonWidget({super.key, required this.title, required this.page});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        // Navigate to the login/register page
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => page),
+        );
+      },
+      child: Text(title),
+    );
+  }
+}
