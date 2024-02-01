cec293d902a50a66564551cda7e131b810286d26
nitay658 <nitayv@post.bgu.ac.il>
2024-02-01 18:40:16 +0200

    compress and save video in gallery

--- lib/poc/Compress.dart
+++ lib/poc/Compress.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gallery_saver/gallery_saver.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:video_compress/video_compress.dart';
@@ -34,78 +35,107 @@ class _VideoCompressorWidgetState extends State<VideoCompressorWidget> {
 
   Future<void> pickVideo() async {
     final pickedFile = await ImagePicker().getVideo(source: ImageSource.gallery);
-    setState(() {
-      videoPath = pickedFile?.path ?? '';
-    });
+    if (pickedFile != null) {
+      setState(() {
+        videoPath = pickedFile.path ?? '';
+      });
+      await compressAndShowDialog();
+    } else {
+      // Handle case where the user canceled video selection
+      print('No video selected.');
+    }
   }
+
 
   Future<void> compressAndShowDialog() async {
-    late String originalSize;
-    late String compressedSize;
-    late String? originalPath;
-    late String? compressedPath;
+    try {
+      MediaInfo? mediaInfoOriginal = await compressVideo(videoPath);
+      MediaInfo? mediaInfoCompress = await compressVideo(videoPath, quality: VideoQuality.LowQuality);
 
-    MediaInfo? mediaInfoOriginal = await VideoCompress.compressVideo(
-      videoPath,
-      // quality: VideoQuality.LowQuality,
-      deleteOrigin: false,
-    );
-    MediaInfo? mediaInfoCompress = await VideoCompress.compressVideo(
-      videoPath,
-      quality: VideoQuality.LowQuality,
-      deleteOrigin: false,
-    );
+      File? compressedFile = mediaInfoCompress?.file;
 
-    File? c = mediaInfoCompress?.file;
-    Directory appDocumentsDirectory = await getApplicationDocumentsDirectory();
-    final compressDirectory = Directory('${appDocumentsDirectory.path}/compress_videos');
+      String originalSize = formatFileSize(mediaInfoOriginal?.filesize ?? 0);
+      String? originalPath = mediaInfoOriginal?.path;
+      String compressedSize = formatFileSize(mediaInfoCompress?.filesize ?? 0);
+      String? compressedPath = compressedFile?.path;
 
-    if (!compressDirectory.existsSync()) {
-      compressDirectory.createSync();
-    }
-    String compressName = videoPath.split('/').last;
-    compressName = compressName.split('.').first;
-    await c?.copy('${compressDirectory.path}/$compressName.mp4');
+      // Update state in a single call
+      setState(() {
+        originalSize = originalSize;
+        originalPath = originalPath;
+        compressedSize = compressedSize;
+        compressedPath = compressedPath;
+      });
 
-    setState(() {
-      originalSize = formatFileSize(mediaInfoOriginal?.filesize ?? 0);
-      originalPath = mediaInfoOriginal?.path;
-      compressedSize = formatFileSize(mediaInfoCompress?.filesize ?? 0);
-      compressedPath = mediaInfoCompress?.path;
-    });
-
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('File Sizes'),
-          content: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text('Original Size: $originalSize'),
-              Text('Original Path: $originalPath'),
-              Text('Compressed Size: $compressedSize'),
-              Text('Compressed Path: $compressedPath'),
-            ],
-          ),
-          actions: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text('OK'),
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('File Sizes'),
+            content: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text('Original Size: $originalSize'),
+                Text('Original Path: $originalPath'),
+                Text('Compressed Size: $compressedSize'),
+                Text('Compressed Path: $compressedPath'),
+              ],
             ),
-          ],
-        );
-      },
+            actions: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+    } catch (e) {
+      print('Error compressing or saving video: $e');
+    }
+  }
+
+
+  Future<MediaInfo?> compressVideo(String videoPath, {VideoQuality quality = VideoQuality.DefaultQuality}) {
+    return VideoCompress.compressVideo(
+      videoPath,
+      quality: quality,
+      deleteOrigin: false,
     );
   }
 
+  Future<void> saveCompressedVideo(File? compressedFile) async {
+    if (compressedFile != null) {
+      try {
+        // Save the compressed video to the gallery
+        await GallerySaver.saveVideo(compressedFile.path);
+      } catch (e) {
+        print('Error saving compressed video: $e');
+        // Handle errors or show a message if necessary
+      }
+    }
+  }
+
+
+
+
   String formatFileSize(int fileSize) {
-    // Implement your logic for formatting file size (e.g., converting bytes to KB, MB, etc.).
-    // For simplicity, this example returns the file size in bytes.
-    return '$fileSize bytes';
+    const int KB = 1024;
+    const int MB = KB * KB;
+    const int GB = MB * KB;
+
+    if (fileSize >= GB) {
+      return '${(fileSize / GB).toStringAsFixed(2)} GB';
+    } else if (fileSize >= MB) {
+      return '${(fileSize / MB).toStringAsFixed(2)} MB';
+    } else if (fileSize >= KB) {
+      return '${(fileSize / KB).toStringAsFixed(2)} KB';
+    } else {
+      return '$fileSize bytes';
+    }
   }
 }
 
