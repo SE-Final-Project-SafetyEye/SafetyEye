e3f10079142cb493faeab3ed6a791c2576232728
t-irozenberg <t-irozenberg@microsoft.com>
2024-05-09 22:56:57 +0300

    test commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -10,6 +12,8 @@ import 'package:safety_eye_app/providers/sensors_provider.dart';
 import 'package:safety_eye_app/providers/signatures_provider.dart';
 import 'package:safety_eye_app/views/screens/auth_screen.dart';
 import 'package:safety_eye_app/views/screens/home_screen.dart';
+import 'package:speech_to_text/speech_recognition_event.dart';
+import 'package:speech_to_text/speech_to_text_provider.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -19,18 +23,31 @@ void main() async {
   );
   List<CameraDescription> cameras = await availableCameras();
   runApp(MultiProvider(
-    providers: [ChangeNotifierProvider(create: (context) => IocContainerProvider())],
+    providers: [
+      ChangeNotifierProvider(create: (context) => IocContainerProvider())
+    ],
     builder: (context, child) {
-      final iocProvider = Provider.of<IocContainerProvider>(context, listen: false);
+      final iocProvider =
+          Provider.of<IocContainerProvider>(context, listen: false);
       return MultiProvider(providers: [
-      ChangeNotifierProvider(create: (context) => iocProvider.container.get<AuthenticationProvider>()),
-      ChangeNotifierProvider(create: (context) => iocProvider.container.get<PermissionsProvider>()),
-      ChangeNotifierProvider(create: (context) => iocProvider.container.get<SensorsProvider>()),
-      ChangeNotifierProvider(create: (context) {
-        return iocProvider.container.get<SettingsProvider>();
-      }),
-      ChangeNotifierProvider(create: (context) => iocProvider.container.get<SignaturesProvider>()),
-    ], child: const MyApp());
+        ChangeNotifierProvider(
+            create: (context) =>
+                iocProvider.container.get<AuthenticationProvider>()),
+        ChangeNotifierProvider(
+            create: (context) =>
+                iocProvider.container.get<PermissionsProvider>()),
+        ChangeNotifierProvider(
+            create: (context) => iocProvider.container.get<SensorsProvider>()),
+        ChangeNotifierProvider(create: (context) {
+          return iocProvider.container.get<SettingsProvider>();
+        }),
+        ChangeNotifierProvider(
+            create: (context) =>
+                iocProvider.container.get<SignaturesProvider>()),
+        ChangeNotifierProvider(
+            create: (context) =>
+                iocProvider.container.get<SpeechToTextProvider>()),
+      ], child: const MyApp());
     },
   ));
 }
@@ -38,11 +55,30 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  void initSpeechProvider(SpeechToTextProvider speechProvider) async {
+    var available = await speechProvider.initialize();
+    StreamSubscription<SpeechRecognitionEvent> _subscription;
+    _subscription = speechProvider.stream.listen((recognitionEvent) {
+      if (recognitionEvent.eventType == SpeechRecognitionEventType.partialRecognitionEvent )  {
+        print("I heard partial: ${recognitionEvent.recognitionResult?.recognizedWords}");
+      }
+      if (recognitionEvent.eventType == SpeechRecognitionEventType.finalRecognitionEvent )  {
+        print("I heard: ${recognitionEvent.recognitionResult?.recognizedWords}");
+      }
+    });
+    if (available) speechProvider.listen();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final authProvider = Provider.of<AuthenticationProvider>(context, listen: true);
-    final permissionsProvider = Provider.of<PermissionsProvider>(context, listen: false);
+    final authProvider =
+        Provider.of<AuthenticationProvider>(context, listen: true);
+    final permissionsProvider =
+        Provider.of<PermissionsProvider>(context, listen: false);
     final settingsProvider = Provider.of<SettingsProvider>(context);
+    final speechProvider =
+        Provider.of<SpeechToTextProvider>(context, listen: false);
+    initSpeechProvider(speechProvider);
 
     return FutureBuilder(
         future: permissionsProvider.init(),
@@ -51,12 +87,15 @@ class MyApp extends StatelessWidget {
             return MaterialApp(
                 title: 'SafetyEye',
                 theme: ThemeData(
-                  colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue, secondary: Colors.blue),
+                  colorScheme: ColorScheme.fromSeed(
+                      seedColor: Colors.blue, secondary: Colors.blue),
                   textTheme: const TextTheme(
                     bodySmall: TextStyle(fontSize: 12.0),
                   ),
                 ),
-                home: !authProvider.isSignedIn() ? const AuthScreen() : HomeScreen(settingsProvider),
+                home: !authProvider.isSignedIn()
+                    ? const AuthScreen()
+                    : HomeScreen(settingsProvider),
                 routes: {
                   "/home": (context) => HomeScreen(settingsProvider),
                   "/auth": (context) => const AuthScreen(),
--- lib/providers/signatures_provider.dart
+++ lib/providers/signatures_provider.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/cupertino.dart';
+import 'package:logger/logger.dart';
 import 'package:safety_eye_app/providers/auth_provider.dart';
 import 'package:safety_eye_app/services/signatures_service.dart';
+import 'package:speech_to_text/speech_to_text_provider.dart';
 
 class SignaturesProvider extends ChangeNotifier {
+  final Logger _logger = Logger();
   final SignaturesService signaturesService;
 
-  SignaturesProvider(AuthenticationProvider authProvider, this.signaturesService) {
+  SignaturesProvider(AuthenticationProvider authProvider, SpeechToTextProvider speechToTextProvider, this.signaturesService) {
     final user = authProvider.currentUser;
     if (user != null) {
       signaturesService.init(authProvider);
     }
     listenToAuth(authProvider);
+    listenToSpeech(speechToTextProvider);
   }
 
   void listenToAuth(AuthenticationProvider authProvider) {
@@ -19,6 +23,12 @@ class SignaturesProvider extends ChangeNotifier {
       if (user != null) {
         signaturesService.init(authProvider);
       }
+    });
+  }
+
+  void listenToSpeech(SpeechToTextProvider speechToTextProvider) {
+    speechToTextProvider.addListener(() {
+      _logger.i('listened to:  ${speechToTextProvider.lastResult?.recognizedWords}');
     });
   }
 }
