69e838595890a313e02d532cea2f6c833792df21
nitay658 <nitayv@post.bgu.ac.il>
2024-05-10 01:23:11 +0300

    backend journeys List

--- lib/providers/journeys_provider.dart
+++ lib/providers/journeys_provider.dart
@@ -3,18 +3,23 @@ import 'dart:io';
 import 'package:flutter/cupertino.dart';
 
 
+import '../poc/uploadVideo/BackendService.dart';
 import '../repositories/file_system_repo.dart';
 import 'auth_provider.dart';
 
 class JourneysProvider extends ChangeNotifier {
-  List<FileSystemEntity> videoFolders = [];
+  List<FileSystemEntity> localVideoFolders = [];
   late AuthenticationProvider authenticationProvider;
   late FileSystemRepository fileSystemRepository;
+  late BackendService backendService;
+  List<String> backendVideoFolders = [];
 
   JourneysProvider({required this.authenticationProvider});
 
   Future<void> initializeJourneys() async {
     fileSystemRepository = FileSystemRepository(authProvider: authenticationProvider);
-    videoFolders = await fileSystemRepository.getVideoList();
+    backendService = BackendService(authenticationProvider.currentUser);
+    localVideoFolders = await fileSystemRepository.getVideoList();
+    backendVideoFolders = (await backendService.getJourneys()) as List<String>; //TODO: check if connection is good
   }
 }
--- lib/views/components/journeys/journeys_content.dart
+++ lib/views/components/journeys/journeys_content.dart
@@ -1,30 +1,98 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:safety_eye_app/providers/journeys_provider.dart';
-
-import '../../../providers/auth_provider.dart';
-import '../../../providers/chunks_provider.dart';
 import '../../../providers/ioc_provider.dart';
 import '../chunks/chunks_content.dart';
 
-class JourneysPage extends StatelessWidget {
+class JourneysPage extends StatefulWidget {
   const JourneysPage({super.key});
 
   @override
+  State<JourneysPage> createState() => _JourneysPageState();
+}
+
+class _JourneysPageState extends State<JourneysPage> {
+  late List<String> selectedBackendJourneys;
+  bool isSelectItem = false;
+
+  @override
+  void initState() {
+    selectedBackendJourneys = [];
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    selectedBackendJourneys = [];
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final journeys = Provider.of<IocContainerProvider>(context, listen: false).container.get<JourneysProvider>();
+    final journeys = Provider.of<IocContainerProvider>(context, listen: false)
+        .container
+        .get<JourneysProvider>();
 
     return FutureBuilder(
         future: journeys.initializeJourneys(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.done) {
-            return Scaffold(body: ListView(
-              children: [
-                _buildVideoList(journeys.videoFolders, 'Video Folders'),
-              ],
-            ),);
+            return Scaffold(
+              body: ListView(
+                children: [
+                  _buildLocalVideoList(journeys.localVideoFolders),
+                  if (journeys.backendVideoFolders.isNotEmpty)
+                    Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          const Text("Backend Journeys"),
+                          ListView.builder(
+                            itemCount: journeys.backendVideoFolders.length,
+                            itemBuilder: (context, index) {
+                              return ListTile(
+                                title:
+                                    Text(journeys.backendVideoFolders[index]),
+                                onTap: () {
+                                  setState(() {
+                                    if (selectedBackendJourneys.contains(
+                                        journeys.backendVideoFolders[index])) {
+                                      selectedBackendJourneys.remove(
+                                          journeys.backendVideoFolders[index]);
+                                    } else {
+                                      selectedBackendJourneys.add(
+                                          journeys.backendVideoFolders[index]);
+                                    }
+                                  });
+                                },
+                                leading: Checkbox(
+                                  value: selectedBackendJourneys.contains(
+                                      journeys.backendVideoFolders[index]),
+                                  onChanged: (value) {
+                                    setState(() {
+                                      if (value != null && value) {
+                                        selectedBackendJourneys.add(journeys
+                                            .backendVideoFolders[index]);
+                                      } else {
+                                        selectedBackendJourneys.remove(journeys
+                                            .backendVideoFolders[index]);
+                                      }
+                                    });
+                                  },
+                                ),
+                              );
+                            },
+                          ),
+                        ]),
+                ],
+              ),
+              floatingActionButton: FloatingActionButton(
+                onPressed: () {
+                  //journeys.setSelectedBackendJourneys(selectedBackendJourneys); //TODO: implement journeys Download
+                },
+                child: const Icon(Icons.cloud_download),
+              ),
+            );
           } else {
             return const CircularProgressIndicator();
           }
@@ -32,33 +100,27 @@ class JourneysPage extends StatelessWidget {
   }
 }
 
-Widget _buildVideoList(List<FileSystemEntity> paths, String title) {
+Widget _buildLocalVideoList(List<FileSystemEntity> paths) {
   return Column(
     crossAxisAlignment: CrossAxisAlignment.stretch,
     children: [
-      Container(
-        padding: const EdgeInsets.all(8.0),
-        child: Text(
-          title,
-          style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-        ),
-      ),
+      const Text("Local Journeys"),
       ListView.builder(
         shrinkWrap: true,
         physics: const NeverScrollableScrollPhysics(),
         itemCount: paths.length,
         itemBuilder: (context, index) {
-          return VideoCard(fileSystemEntity: paths[index]);
+          return LocalVideoCard(fileSystemEntity: paths[index]);
         },
       ),
     ],
   );
 }
 
-class VideoCard extends StatelessWidget {
+class LocalVideoCard extends StatelessWidget {
   final FileSystemEntity fileSystemEntity;
 
-  const VideoCard({super.key, required this.fileSystemEntity});
+  const LocalVideoCard({super.key, required this.fileSystemEntity});
 
   @override
   Widget build(BuildContext context) {
@@ -68,8 +130,8 @@ class VideoCard extends StatelessWidget {
         Navigator.push(
             context,
             MaterialPageRoute(
-                builder: (context) => ChunksPage(path: fileSystemEntity.path),
-                    ));
+              builder: (context) => ChunksPage(path: fileSystemEntity.path),
+            ));
       },
       child: Card(
         child: Column(
