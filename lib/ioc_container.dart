ce34bc352d6ed3eaec5666cdfe2b482fbaecd90d
Edan Levy <62389779+EdanLevy@users.noreply.github.com>
2024-05-26 21:03:52 +0300

    Icon added

--- lib/ioc_container.dart
+++ lib/ioc_container.dart
@@ -14,7 +14,6 @@ extension IocContainerBuilderExtension on IocContainerBuilder {
       ..addSingleton<SensorsProvider>((container) => SensorsProvider())
       ..addSingleton<SettingsProvider>(
           (container) => SettingsProvider(container.get<PreferencesService>()))
-      ..addSingleton<SettingsProvider>((container) => SettingsProvider(container.get<PreferencesService>()))
       ..addSingleton<SpeechToTextProvider>((container) => SpeechToTextProvider(SpeechToText()))
 
       ..add<VideoRecordingProvider>((container) {
