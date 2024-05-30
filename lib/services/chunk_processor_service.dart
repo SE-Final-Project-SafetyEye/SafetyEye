c1a5637cb06ae30a12613db9c94f0b48d82bdaed
nitay658 <nitayv@post.bgu.ac.il>
2024-05-30 13:05:22 +0300

    sign and frames works

--- lib/services/chunk_processor_service.dart
+++ lib/services/chunk_processor_service.dart
@@ -5,46 +5,57 @@ import 'package:camera/camera.dart';
 import 'package:flutter_ffmpeg/flutter_ffmpeg.dart';
 import 'package:logger/logger.dart';
 import 'package:safety_eye_app/providers/providers.dart';
+import 'package:uuid/uuid.dart';
 import '../repositories/file_system_repo.dart';
 
 class ChunkProcessorService {
   final FileSystemRepository fileSystemRepository;
   final SignaturesProvider signaturesProvider;
   final Logger _logger = Logger();
+  var uuid = const Uuid();
 
   ChunkProcessorService({
     required this.fileSystemRepository,
     required this.signaturesProvider,
   });
 
-  Future<void> processChunk(XFile videoChunk, int chunkNumber) async {
+  Future<void> processChunk(
+      XFile videoChunk, int chunkNumber, String jsonMetaData) async {
     try {
-      // Read the video chunk as bytes
       Uint8List videoBytes = await videoChunk.readAsBytes();
-
       Directory dir =
           await fileSystemRepository.stopRecording(videoChunk, chunkNumber);
-      await extractFrames(dir.path);
-      //todo: replace this with the actual file name
-      await signaturesProvider.sign("ID in here",base64Encode(videoBytes));
+      File jsonFile = await fileSystemRepository.saveDataToFile(jsonMetaData);
+      XFile jsonXFile = XFile(jsonFile.path);
+      Uint8List jsonBytes = await jsonXFile.readAsBytes();
+      await extractFrames(dir);
+      String jsonMetaDataID = _extractUuidFromFileName(jsonXFile.name);
+      await signaturesProvider.sign(jsonMetaDataID, base64Encode(jsonBytes));
+      XFile xFile = XFile(dir.path);
+      String videoID = _extractUuidFromFileName(xFile.name);
+      _logger.i("video Id: $videoID");
+      await signaturesProvider.sign(videoID, base64Encode(videoBytes));
     } catch (e) {
       _logger.e('Error processing chunk: $e');
     }
   }
 
-  Future<void> extractFrames(String outputDir) async {
+  Future<void> extractFrames(Directory outputDir) async {
     try {
       FlutterFFmpeg ffmpeg = FlutterFFmpeg();
 
-      int intervalInSeconds = 5;
+      // Generate a unique UUID for each extraction
+      var uuidG = const Uuid().v4();
+      _logger.i("outputDir: ${outputDir.path}");
 
-      _logger.i("outputDir:  $outputDir");
+      // Use the correct FFmpeg command to extract frames with the desired output file format
       int rc = await ffmpeg.execute(
-          '-i $outputDir -vf fps=1/$intervalInSeconds ${outputDir}frame-%03d.jpg');
+        '-i ${outputDir.path} -vf fps=1/5 "${outputDir.parent.path}/extractFrame_$uuidG-%03d.jpg"',
+      );
 
       if (rc == 0) {
-        _logger.i('Frames extracted successfully: $outputDir');
-        await signVideoFrames(outputDir);
+        _logger.i('Frames extracted successfully: ${outputDir.path}');
+        await signVideoFrames(outputDir.path);
       } else {
         _logger.e('Error extracting frames: $rc');
       }
@@ -53,27 +64,42 @@ class ChunkProcessorService {
     }
   }
 
+
+
   Future<void> signVideoFrames(String outputDir) async {
     File file = File(outputDir);
     String directoryPath = file.parent.path;
-    Directory outputDirectory = Directory(directoryPath);
-    List<FileSystemEntity> frames = [];
-    List<FileSystemEntity> files = outputDirectory.listSync();
-    for (FileSystemEntity file in files) {
-      FileStat fileStat = file.statSync();
-      if (fileStat.type == FileSystemEntityType.file) {
-        if (file.path.endsWith('.jpg')) {
-          frames.add(file);
-        }
+    Directory directory = Directory(directoryPath);
+    List<File> frames = [];
+
+    if (!directory.existsSync()) {
+      return;
+    }
+
+    List<FileSystemEntity> files = directory.listSync(recursive: true);
+
+    for (FileSystemEntity entity in files) {
+      if (entity is File && entity.path.endsWith('.jpg')) {
+        frames.add(entity);
       }
     }
-    _logger.i("outputDirectory");
-    // Sign and save each frame
     for (var entity in frames) {
-      File frameFile = File(entity.path);
+      XFile frameFile = XFile(entity.path);
       Uint8List frameBytes = await frameFile.readAsBytes();
-      //todo: replace with the actual id
-      await signaturesProvider.sign("Add If Here",base64Encode(frameBytes));
+      String frameID = _extractUuidFromFileName(frameFile.name);
+      _logger.i("video Id: $frameID");
+      await signaturesProvider.sign(frameID, base64Encode(frameBytes));
+    }
+  }
+
+  String _extractUuidFromFileName(String fileName) {
+    List<String> parts = fileName.split('_');
+
+    if (parts.length > 1) {
+      String uuidPart = parts[1];
+      return uuidPart.split('.').first; // Remove the "." extension if present
+    } else {
+      return '';
     }
   }
 }
